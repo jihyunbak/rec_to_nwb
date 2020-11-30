177ce7a0dbae8bf5f7f00df13c478c3ff50f1a7c
Ji Hyun Bak <jihyun.bak@gmail.com>
2020-11-29 16:29:09 -0800

    temporary attempt to fill in timestamps; did not work

--- rec_to_nwb/processing/nwb/components/video_files/old_fl_video_files_extractor.py
+++ rec_to_nwb/processing/nwb/components/video_files/old_fl_video_files_extractor.py
@@ -17,8 +17,20 @@ class OldFlVideoFilesExtractor:
         for video_file in video_files:
             new_fl_video_file = {
                 "name": video_file["name"],
-                "timestamps": np.array([]),
+                # "timestamps": np.array([]),
+                "timestamps": self.__fill_timestamps(readTrodesExtractedDataFile(
+                    self.raw_data_path + "/"
+                    + video_file["name"][:-4]
+                    + "videoTimeStamps.cameraHWFrameCount"
+                )["data"]),
                 "device": video_file["camera_id"]
             }
             extracted_video_files.append(new_fl_video_file)
         return extracted_video_files
+
+    def __fill_timestamps(self, timestamps):
+        converted_timestamps = np.ndarray(shape=np.shape(timestamps), dtype='float64')
+        for i, _ in enumerate(timestamps):
+            value = float('nan')  # just a dummy value for now
+            converted_timestamps[i] = value
+        return converted_timestamps
