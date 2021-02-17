9ed1b71573e3d2ec1cacfb98177e73496009073d
Ji Hyun Bak <jhbak@berkeley.edu>
2021-02-16 16:19:17 -0800

    keep parallel structure in [old_]analog_originator

--- rec_to_nwb/processing/builder/originators/old_analog_originator.py
+++ rec_to_nwb/processing/builder/originators/old_analog_originator.py
@@ -1,7 +1,6 @@
 from rec_to_nwb.processing.nwb.components.analog.analog_creator import AnalogCreator
 from rec_to_nwb.processing.nwb.components.analog.analog_files import AnalogFiles
 from rec_to_nwb.processing.nwb.components.analog.analog_injector import AnalogInjector
-from rec_to_nwb.processing.nwb.components.analog.fl_analog_manager import FlAnalogManager
 from rec_to_nwb.processing.nwb.components.analog.old_fl_analog_manager import OldFlAnalogManager
 
 
@@ -10,6 +9,7 @@ class OldAnalogOriginator:
     def __init__(self, datasets, metadata):
         self.datasets = datasets
         self.metadata = metadata
+        self.continuous_time_files = self.__get_continuous_time_files()
 
     def make(self, nwb_content):
         analog_directories = [single_dataset.get_data_path_from_dataset('analog') for single_dataset in self.datasets]
@@ -18,8 +18,10 @@ class OldAnalogOriginator:
             analog_files=analog_files.get_files(),
         )
         fl_analog = old_analog_manager.get_analog()
-        old_analog_injector = AnalogInjector(nwb_content)
-        old_analog_injector.inject(AnalogCreator.create(fl_analog, self.metadata['units']['analog']), 'analog')
+        analog_injector = AnalogInjector(nwb_content)
+        analog_injector.inject(AnalogCreator.create(fl_analog, self.metadata['units']['analog']), 'analog')
 
     def __get_continuous_time_files(self):
-        return [single_dataset.get_continuous_time() for single_dataset in self.datasets]
+        # return [single_dataset.get_continuous_time() for single_dataset in self.datasets]
+        # for old dataset (check!)
+        return [None for single_dataset in self.datasets]
